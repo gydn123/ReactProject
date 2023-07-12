8cf9bf017f80bab83ca61fb2e55ea42a7fc299fa
gydn <gydn123@naver.com>
2023-07-12 12:51:36 +0900

    2023-07-12

--- src/component/orderlist.js
+++ src/component/orderlist.js
@@ -22,28 +22,42 @@ function Orderlist() {
     }
   };
   console.log(data);
-  const refund = () => {
-    console.log("a");
+  const handleRefund = (order_id) => {
+    if (window.confirm("확불 하시겠습니까?")) {
+      const url = "http://localhost:8080/refund";
+      const refunddata = {
+        order_id: order_id,
+      };
+      axios.post(url, refunddata).then((response) => {
+        alert("환불 처리 되었습니다.");
+        window.location.reload();
+      });
+      console.log("a");
+    }
   };
 
   const handleConfirm = (order_id, use_point, member_id) => {
-    const url = "http://localhost:8080/check";
-    const confirmdata = {
-      order_id: order_id,
-      use_point: use_point,
-      member_id: member_id,
-    };
-    axios
-      .post(url, confirmdata)
-      .then((response) => {
-        console.log(response.data);
-        alert("구매해 주셔서 감사합니다!");
-
-        navigate("/check");
-      })
-      .catch((error) => {
-        console.error("Error occurred during request:", error);
-      });
+    if (window.confirm("구매확정 하시겠습니까?")) {
+      const myPointInput = document.getElementById("myPointInput");
+      const m_point = myPointInput.value;
+      const url = "http://localhost:8080/check";
+      const confirmdata = {
+        order_id: order_id,
+        use_point: Number(use_point).toFixed(2),
+        member_id: member_id,
+        m_point: Number(m_point).toFixed(2),
+      };
+      axios
+        .post(url, confirmdata)
+        .then((response) => {
+          console.log(response.data);
+          alert("구매해 주셔서 감사합니다!");
+          window.location.reload();
+        })
+        .catch((error) => {
+          console.error("Error occurred during request:", error);
+        });
+    }
   };
 
   // Locale 설정
@@ -55,113 +69,155 @@ function Orderlist() {
   };
   return (
     <div className="container custom-main-padding border-bottom">
-      {data.orderlist.map((d, index) => (
-        <div className="row" key={`${d.order_id}-${index}`}>
-          {d.quantity !== 0 && (
-            <>
-              <div className="w-100"></div>
-              <h4 className="mb-3">
-                주문번호: {d.order_id}
-                {d.checkorder ? (
-                  <button
-                    className="btn btn-success"
-                    disabled={d.checkorder || d.checkrefund}
+      {Object.values(
+        data.orderlist.reduce((acc, d) => {
+          if (!acc[d.order_id]) {
+            acc[d.order_id] = [];
+          }
+          acc[d.order_id].push(d);
+          return acc;
+        }, {})
+      )
+        .sort((a, b) => new Date(b[0].order_date) - new Date(a[0].order_date))
+        .map((orders, index) => (
+          <div className="row" key={`${orders[0].order_date}-${index}`}>
+            {orders[0].quantity !== 0 && (
+              <>
+                <div className="w-100"></div>
+                <h4 className="mb-3">
+                  주문번호: {orders[0].order_id}
+                  {orders[0].checkorder ? (
+                    <button
+                      className="btn btn-success"
+                      disabled={orders[0].checkorder || orders[0].checkrefund}
+                    >
+                      구매 확정 완료
+                    </button>
+                  ) : orders[0].checkrefund ? (
+                    <button
+                      className="btn btn-danger"
+                      disabled={orders[0].checkorder || orders[0].checkrefund}
+                    >
+                      환불 처리 완료
+                    </button>
+                  ) : (
+                    <>
+                      <button
+                        className="btn btn-outline-success"
+                        onClick={() =>
+                          handleConfirm(
+                            orders[0].order_id,
+                            orders[0].use_point,
+                            orders[0].member_id
+                          )
+                        }
+                        disabled={orders[0].checkorder || orders[0].checkrefund}
+                      >
+                        구매 확정
+                      </button>
+                      <button
+                        className="btn btn-outline-danger"
+                        onClick={() => handleRefund(orders[0].order_id)}
+                        disabled={orders[0].checkorder || orders[0].checkrefund}
+                      >
+                        환불
+                      </button>
+                    </>
+                  )}
+                </h4>
+                {orders.map((d, idx) => (
+                  <div
+                    className="col-md-4 mb-5 "
+                    key={`${orders[0].order_id}-${d.ticket_name}-${idx}`}
                   >
-                    구매 확정 완료
-                  </button>
-                ) : d.checkrefund ? (
-                  <button
-                    className="btn btn-danger"
-                    disabled={d.checkorder || d.checkrefund}
-                  >
-                    환불 처리 완료
-                  </button>
-                ) : (
-                  <button
-                    className="btn btn-outline-success"
-                    onClick={() =>
-                      handleConfirm(d.order_id, d.use_point, d.member_id)
-                    }
-                    disabled={d.checkorder || d.checkrefund}
-                  >
-                    구매 확정 하러 가기
-                  </button>
-                )}
-              </h4>
-              <div className="col-md-4 mb-5 ">
-                <div className="card">
-                  <div className="card-body">
-                    <h5 className="card-title">주문번호 : {d.order_id}</h5>
-                    <p className="card-text">{d.a_name}</p>
-                    <p className="card-text">{d.promotion_name}</p>
-                    <p className="card-text">{d.ticket_name}</p>
-                    <p className="card-subtitle mb-2 text-muted">
-                      {toKRWString(d.ticket_price * (1 - d.discount))} *{" "}
-                      {d.quantity}
-                    </p>
-                    {d.checkorder && d.use_point !== null && (
-                      <>
-                        <p className="card-text">
-                          결제금액 :{" "}
-                          {toKRWString(
-                            d.ticket_price * (1 - d.discount) * d.quantity -
-                              d.use_point
-                          )}
+                    <div className="card">
+                      <div className="card-body">
+                        <h5 className="card-title">주문번호 : {d.order_id}</h5>
+                        <p className="card-text">{d.a_name}</p>
+                        <p className="card-text">{d.promotion_name}</p>
+                        <p className="card-text">{d.ticket_name}</p>
+                        <p className="card-subtitle mb-2 text-muted">
+                          {toKRWString(d.ticket_price * (1 - d.discount))} *{" "}
+                          {d.quantity}
                         </p>
-                        <p className="text-danger">
-                          포인트 사용 : {toKRWString(d.use_point)}
-                        </p>
-                      </>
-                    )}
-                    {d.checkorder &&
-                      (d.use_point === null || d.use_point === 0) && (
-                        <p className="card-text">
-                          결제금액 :{" "}
-                          {toKRWString(
-                            d.ticket_price * (1 - d.discount) * d.quantity
-                          )}
-                        </p>
-                      )}
-                    {d.checkrefund && (
-                      <del>
-                        <p className="card-text">
-                          결제금액 :{" "}
-                          {toKRWString(
-                            d.ticket_price * (1 - d.discount) * d.quantity
-                          )}
-                        </p>
-                      </del>
-                    )}
-                    {d.checkorder && (
-                      <p className="text-danger">
-                        포인트 적립 :{" "}
-                        {toKRWString(
-                          d.ticket_price * (1 - d.discount) * d.quantity * 0.05
+                        <input
+                          id="myPointInput"
+                          className="MyPoint"
+                          type="hidden"
+                          value={
+                            d["ticket_price"] *
+                            (1 - d["discount"]) *
+                            d["quantity"] *
+                            0.05
+                          }
+                        />
+                        {d.checkorder && d.use_point !== null && (
+                          <>
+                            <p className="card-text">
+                              결제금액 :{" "}
+                              {toKRWString(
+                                d.ticket_price * (1 - d.discount) * d.quantity -
+                                  d.use_point
+                              )}
+                            </p>
+                            <p className="text-danger">
+                              포인트 사용 : {toKRWString(d.use_point)}
+                            </p>
+                          </>
                         )}
-                      </p>
-                    )}
-                    {d.checkrefund && (
-                      <del>
-                        <p className="text-danger">
-                          포인트 적립 :{" "}
-                          {toKRWString(
-                            d.ticket_price *
-                              (1 - d.discount) *
-                              d.quantity *
-                              0.05
+                        {d.checkorder &&
+                          (d.use_point === null || d.use_point === 0) && (
+                            <p className="card-text">
+                              결제금액 :{" "}
+                              {toKRWString(
+                                d.ticket_price * (1 - d.discount) * d.quantity
+                              )}
+                            </p>
                           )}
-                        </p>
-                      </del>
-                    )}
-                    <p className="card-text">구매 날짜 : {d.order_date}</p>
+                        {d.checkrefund && (
+                          <del>
+                            <p className="card-text">
+                              결제금액 :{" "}
+                              {toKRWString(
+                                d.ticket_price * (1 - d.discount) * d.quantity
+                              )}
+                            </p>
+                          </del>
+                        )}
+                        {d.checkorder && (
+                          <p className="text-danger">
+                            포인트 적립 :{" "}
+                            {toKRWString(
+                              d.ticket_price *
+                                (1 - d.discount) *
+                                d.quantity *
+                                0.05
+                            )}
+                          </p>
+                        )}
+                        {d.checkrefund && (
+                          <del>
+                            <p className="text-danger">
+                              포인트 적립 :{" "}
+                              {toKRWString(
+                                d.ticket_price *
+                                  (1 - d.discount) *
+                                  d.quantity *
+                                  0.05
+                              )}
+                            </p>
+                          </del>
+                        )}
+                        <p className="card-text">구매 날짜 : {d.order_date}</p>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
-            </>
-          )}
-          <div></div>
-        </div>
-      ))}
+                ))}
+              </>
+            )}
+            <div></div>
+          </div>
+        ))}
     </div>
   );
 }
